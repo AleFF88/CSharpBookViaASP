8858cabe78f9ca40726c5283d6470b8dde7be3e9
AleFF88 <1871847@gmail.com>
2025-04-20 14:34:45 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSharpBookViaASP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
