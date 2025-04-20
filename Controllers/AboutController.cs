04b5ee1dcd136203a52a1b6b40d682c3e2d66b29
AleFF88 <1871847@gmail.com>
2025-04-20 17:02:21 +0300

    "About" controller added

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpBookViaASP.Controllers {
+    public class AboutController : Controller {
+        public IActionResult About() {
+            return View();
+        }
+    }
+}
