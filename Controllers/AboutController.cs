84ff1d05e1716b7fdefc9d99695d2644c139b3ce
AleFF88 <1871847@gmail.com>
2025-04-20 18:04:23 +0300

    About controller

--- Controllers/AboutController.cs
+++ Controllers/AboutController.cs
@@ -1,9 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSharpBookViaASP.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CSharpBookViaASP.Controllers {
     public class AboutController : Controller {
         public IActionResult About() {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Check(AboutModel input) {
+            if (ModelState.IsValid) {
+                Console.Beep();
+            }
+            Console.Beep(1000, 1000);
+
+
+            return View("About");
         }
     }
 }
---
+++ Models/AboutModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CSharpBookViaASP.Models {
+
+    public class AboutModel {
+        [Required (ErrorMessage = "You need input something.")]
+        [Display(Name = "Input description.")]
+        [StringLength(6,ErrorMessage ="Text should be more then 6 symbols.")]
+        public string Description { get; set; }
+    }
+}
