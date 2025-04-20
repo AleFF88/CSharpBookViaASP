ac41dc8cc96e7d19ff19f9b0179145df620cfa60
AleFF88 <1871847@gmail.com>
2025-04-20 19:07:24 +0300

    Sidebar implemented

---
+++ Controllers/SidebarController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSharpBookViaASP.Controllers {
+    public class SidebarController : Controller {
+
+        public IActionResult Arrays() {
+            ViewData["Message"] = "Это Arrays из SidebarController.";
+            return View();
+        }
+        public IActionResult NewArticle() {
+            ViewData["Message"] = "Это NewArticle из SidebarController.";
+            return View();
+        }
+    }
+}
